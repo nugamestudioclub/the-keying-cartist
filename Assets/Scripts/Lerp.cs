22cf0c13df9e4bc03d3ba4f1be404b9947bdb802
strongpizzaboy <118563501+strongpizzaboy@users.noreply.github.com>
2025-09-27 18:23:53 -0400

    Update Lerp.cs

--- Assets/Scripts/Lerp.cs
+++ Assets/Scripts/Lerp.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Lerp : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject entity;
+    public float duration = 60f;
+    private float elapsed = 0;
+    private float t;
+    private Vector3 start;
+    private Vector3 end;
     // Start is called before the first frame update
     void Start()
     {
-        
+        start = new Vector3(entity.transform.position.x, entity.transform.position.y, entity.transform.position.z);
+        end = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsed += Time.deltaTime;
+        t = elapsed / duration;
+        entity.transform.position = Vector3.Lerp(start, end, t);
+
     }
 }
