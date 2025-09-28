08295cf9701da092221ca7dae5b4663e19274e64
sudzoof <sun.jos@northeastern.edu>
2025-09-28 02:35:16 -0400

    Translate scale for score checking

--- Assets/Scripts/Scoring.cs
+++ Assets/Scripts/Scoring.cs
@@ -9,7 +9,6 @@ public class Scoring
     {
         var drawing_2d = new Texture2D(drawing.width, drawing.height, TextureFormat.ARGB32, false);
 
-
         RenderTexture.active = drawing;
         drawing_2d.ReadPixels(new Rect(0, 0, drawing.width, drawing.height), 0, 0);
         drawing_2d.Apply();
@@ -17,13 +16,20 @@ public class Scoring
 
         uint hits = 0;
         uint total = 0;
+
+        // Assume source texture is centered and scaled to render texture
+
+        float xScale = (float) drawing.width / source.width;
+        float yScale = (float) drawing.height / source.height;
+        Debug.Log($"xScale: {xScale}, yScale: {yScale}");
+
         for (int i = 0; i < source.height; i++)
         {
             for (int j = 0; j < source.width; j++)
             {
                 bool source_has_pixel = source.GetPixel(j, i).a > 0f;
 
-                if (source_has_pixel && drawing_2d.GetPixel(j, i).a > 0f)
+                if (source_has_pixel && drawing_2d.GetPixel(Mathf.FloorToInt(j * xScale), Mathf.FloorToInt(i * yScale)).a > 0f)
                 {
                     hits++;
                 }
